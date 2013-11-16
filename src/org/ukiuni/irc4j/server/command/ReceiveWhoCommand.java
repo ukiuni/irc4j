99a5ef16a012ac83948e1af6ec6839bf02d84576
ukiuni <ukiuni@gmail.com>
2013-11-16 23:21:28 +0900

    bug fix(?): 	WHO command to user appended.

--- src/org/ukiuni/irc4j/server/command/ReceiveWhoCommand.java
+++ src/org/ukiuni/irc4j/server/command/ReceiveWhoCommand.java
@@ -2,6 +2,7 @@ package org.ukiuni.irc4j.server.command;
 
 import java.util.List;
 
+import org.ukiuni.irc4j.Channel;
 import org.ukiuni.irc4j.IRCEventHandler;
 import org.ukiuni.irc4j.User;
 import org.ukiuni.irc4j.server.ClientConnection;
@@ -14,19 +15,33 @@ public class ReceiveWhoCommand extends ServerCommand {
 	@Override
 	public void execute(IRCServer ircServer, ClientConnection con, List<IRCEventHandler> handlers) throws Throwable {
 		if (1 != getCommandParameters().length) {
-			con.sendPrivateCommand("WHO must specify channel name");
+			con.sendPrivateCommand("WHO must specify name");
 			return;
 		}
-		String channelName = getCommandParameters()[0];
-		ServerChannel channel = ircServer.getChannel(channelName);
-		if (null == channel) {
-			con.sendPrivateCommand("No such channel " + channelName);
-			return;
+		String targetName = getCommandParameters()[0];
+		if (targetName.startsWith("#")) {
+			ServerChannel channel = ircServer.getChannel(targetName);
+			if (null == channel) {
+				con.sendPrivateCommand("No such channel " + targetName);
+				return;
+			}
+			for (User user : channel.getCurrentUserList()) {
+				con.sendCommand("352 " + channel.getName() + " " + user.getName() + " " + user.getHostName() + " " + ircServer.getServerName() + " " + user.getNickName() + " H :0 " + (null != user.getDescription() ? user.getDescription() : ""));
+			}
+			con.send("315 " + targetName + " :End of /WHO list.");
+		} else {
+			ClientConnection targetConnection = ircServer.findConnection(targetName);
+			if (null == targetConnection) {
+				con.sendPrivateCommand("No such user " + targetName);
+				return;
+			}
+			StringBuilder channelNames = new StringBuilder();
+			for (Channel channel : targetConnection.getJoinedChannels()) {
+				channelNames.append(channel.getName() + ",");
+			}
+			User user = targetConnection.getUser();
+			con.sendCommand("352 " + channelNames.substring(0, channelNames.length() - 1) + " " + user.getName() + " " + user.getHostName() + " " + ircServer.getServerName() + " " + user.getNickName() + " H :0 " + (null != user.getDescription() ? user.getDescription() : ""));
+			con.send("315 " + targetName + " :End of /WHO list.");
 		}
-		for (User user : channel.getCurrentUserList()) {
-			con.sendCommand("352 " + con.getNickName() + " " + channel.getName() + " " + user.getName() + " " + user.getHostName() + " " + ircServer.getServerName() + " " + user.getNickName() + " H :0 " + (null != user.getDescription() ? user.getDescription() : ""));
-		}
-		con.send("315 " + con.getNickName() + " " + channelName + " :End of /WHO list.");
 	}
-
 }
