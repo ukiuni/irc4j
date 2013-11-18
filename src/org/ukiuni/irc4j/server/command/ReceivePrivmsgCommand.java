94dc17e3d27eb5483c017cc08b635988ff0dbebc
ukiuni <ukiuni@gmail.com>
2013-11-18 13:09:17 +0900

    new: 	Can DCC to Web Client.

--- src/org/ukiuni/irc4j/server/command/ReceivePrivmsgCommand.java
+++ src/org/ukiuni/irc4j/server/command/ReceivePrivmsgCommand.java
@@ -1,17 +1,21 @@
 package org.ukiuni.irc4j.server.command;
 
+import java.io.IOException;
 import java.util.List;
 
+import org.ukiuni.irc4j.Conf;
 import org.ukiuni.irc4j.IRCEventHandler;
+import org.ukiuni.irc4j.Log;
 import org.ukiuni.irc4j.server.ClientConnection;
 import org.ukiuni.irc4j.server.IRCServer;
 import org.ukiuni.irc4j.server.ServerChannel;
 import org.ukiuni.irc4j.server.ServerCommand;
+import org.ukiuni.irc4j.server.command.FileRecieveThread.OnCompleteListener;
 
 public class ReceivePrivmsgCommand extends ServerCommand {
 
 	@Override
-	public void execute(IRCServer ircServer, ClientConnection selfClientConnection, List<IRCEventHandler> handlers) throws Throwable {
+	public void execute(final IRCServer ircServer, final ClientConnection selfClientConnection, List<IRCEventHandler> handlers) throws Throwable {
 		String[] targets = getCommandParameters()[0].split(",");
 		String message = getCommandParametersString().split(" ", 2)[1];
 		if (message.startsWith(":")) {
@@ -30,7 +34,39 @@ public class ReceivePrivmsgCommand extends ServerCommand {
 			} else if (target.equals(ircServer.getServerName())) {
 				String parameterString = getCommandParametersString().replace(new String(new char[] { new Character((char) 1).charValue() }), "");
 				if (parameterString.startsWith(ircServer.getServerName() + " :DCC")) {
-					new FileRecieveThread(ircServer, selfClientConnection, parameterString).start();
+					final String[] param = parameterString.split(" ");
+					String fileName = param[3];
+					int portNum = Integer.valueOf(param[5]);
+					long fileSize = Long.valueOf(param[6]);
+					final ServerChannel channel = selfClientConnection.getCurrentFileUploadChannel();
+					new FileRecieveThread(selfClientConnection.getUser().getHostName(), portNum, fileName, fileSize, new OnCompleteListener() {
+						@Override
+						public void onComplete(String uploadedUri) {
+							try {
+								selfClientConnection.setCurrentFileUploadChannel(null);
+								String responseMessage = "file upload to " + Conf.getHttpServerURL() + uploadedUri;
+								channel.sendMessage("PRIVMSG", selfClientConnection, responseMessage);
+								selfClientConnection.sendMessage("PRIVMSG", selfClientConnection, channel, responseMessage);
+								selfClientConnection.sendPartCommand(ircServer.getFQSN(), channel.getName(), "upload is completed.");
+							} catch (IOException e) {
+								Log.log("fail after upload", e);
+								try {
+									selfClientConnection.sendMessage("PRIVMSG", selfClientConnection, channel, ":uploaded. but have some fault " + e);
+								} catch (IOException e1) {
+								}
+							}
+						}
+
+						@Override
+						public void onError(Throwable e) {
+							Log.log("Upload fault", e);
+							try {
+								selfClientConnection.sendMessage("PRIVMSG", selfClientConnection, channel, ":uploaded. but have some fault " + e);
+							} catch (IOException e1) {
+								Log.log(e);
+							}
+						}
+					}).start();
 				}
 			} else {
 				ClientConnection clientConnection = ircServer.findConnection(target);
--- src/org/ukiuni/irc4j/server/worker/webworker/WebWorkerClientConnection.java
+++ src/org/ukiuni/irc4j/server/worker/webworker/WebWorkerClientConnection.java
@@ -7,10 +7,13 @@ import java.util.LinkedList;
 import java.util.List;
 
 import org.ukiuni.irc4j.Channel;
+import org.ukiuni.irc4j.Conf;
 import org.ukiuni.irc4j.Log;
 import org.ukiuni.irc4j.server.ClientConnection;
 import org.ukiuni.irc4j.server.Event;
 import org.ukiuni.irc4j.server.IRCServer;
+import org.ukiuni.irc4j.server.command.FileRecieveThread;
+import org.ukiuni.irc4j.server.command.FileRecieveThread.OnCompleteListener;
 
 public class WebWorkerClientConnection extends ClientConnection {
 	List<Event> eventList = Collections.synchronizedList(new LinkedList<Event>());
@@ -46,8 +49,42 @@ public class WebWorkerClientConnection extends ClientConnection {
 	}
 
 	@Override
-	public void sendPrivateMessage(String type, ClientConnection senderClientConnection, String message) throws IOException {
-		send(Event.createMessage(senderClientConnection.getNickName(), senderClientConnection.getNickName(), message));
+	public void sendPrivateMessage(String type, final ClientConnection senderClientConnection, String message) throws IOException {
+		String replacedMessage = message.replace(new String(new char[] { new Character((char) 1).charValue() }), "");
+		if (!replacedMessage.startsWith("DCC SEND ")) {
+			send(Event.createMessage(senderClientConnection.getNickName(), senderClientConnection.getNickName(), message));
+		} else {
+			final String[] param = replacedMessage.split(" ");
+			String fileName = param[2];
+			int portNum = Integer.valueOf(param[4]);
+			long fileSize = Long.valueOf(param[5]);
+			new FileRecieveThread(senderClientConnection.getUser().getHostName(), portNum, fileName, fileSize, new OnCompleteListener() {
+				@Override
+				public void onComplete(String uploadedUri) {
+					try {
+						String responseMessage = Conf.getHttpServerURL() + uploadedUri;
+						send(Event.createMessage(senderClientConnection.getNickName(), senderClientConnection.getNickName(), responseMessage));
+					} catch (Exception e) {
+						Log.log("have fault after upload");
+						try {
+							senderClientConnection.sendNotice(":uploaded but some fault " + e);
+						} catch (IOException e1) {
+							Log.log(e);
+						}
+					}
+				}
+
+				@Override
+				public void onError(Throwable e) {
+					Log.log("Upload fault", e);
+					try {
+						senderClientConnection.sendNotice(":upload fault " + e);
+					} catch (IOException e1) {
+						Log.log(e);
+					}
+				}
+			}).start();
+		}
 	}
 
 	public void sendPrivateSelfMessage(String type, ClientConnection tailingClientConnection, String message) throws IOException {
