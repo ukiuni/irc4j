0b517287c0f195fef4f1eb5bc46f4536ff26b149
ukiuni <ukiuni@gmail.com>
2013-11-25 19:18:04 +0900

    refactor: 	extract message

--- webContents/resource/translate.js
+++ webContents/resource/translate.js
@@ -6,8 +6,10 @@
 	"pluginInfoAboutPluginFunctionTitle":"Pluginで利用可能なfunction",
 	"pluginInfoAboutMethodExecute":"execute(server, client, params)",
 	"pluginInfoAboutMethodExecuteMessage":"このメソッドは、commandで指定されたコマンドがIRCクライアントから送信された時に実行されます。serverはgetChannel等でサーバの情報を取得できます。clientは送信元の情報が取得できます。paramsはIRCクライアントから送信された情報をスペースで分割した配列です。",
+	"pluginInfoAboutMethodExecuteMessageSmall":"サーバーメッセージを返信します。",
+	"pluginInfoAboutMethodExecuteMessageSmall2":"指定したチャネルにメッセージを送信します。<br>\"/COMMAND channelName message\" というフォーマットを期待した実装です。",
 	"pluginInfoAboutMethodSetTimeout":"setTimeout(function, delayTime)",
 	"pluginInfoAboutMethodSetTimeoutMessage":"delayTimeミリ秒後にfunctionを実行します。",
-
+	"pluginInfoAboutMethodSetTimeoutMessageSmall":"10秒後にサーバーメッセージを返信します。",
 	"pluginPaneLinkToInfo":"Pluginの説明"
 }
