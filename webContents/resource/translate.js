6462ae8b40ef88d8a0245450a911601d27164aad
ukiuni <ukiuni@gmail.com>
2013-11-25 18:22:14 +0900

    Update plugin description.

--- webContents/resource/translate.js
+++ webContents/resource/translate.js
@@ -1,8 +1,9 @@
 {
-	"pluginInfoTitle":"日本語は？",
+	"pluginInfoTitle":"Plugin",
 	"pluginInfoSubtitle":"AIRCはPluginで機能を拡張することができます。",
 	"pluginInfoAboutPluginTitle":"Pluginの作成方法",
 	"pluginInfoAboutPlugin":"タイトルバーのメニューのPluginを押してください。<br/>create new ボタンを押すと、新しくPluginを作成することができます。<br/>commandで指定したコマンドをIRCクライアントから実行すると、scriptで定義したexecuteファンクションが実行されます。",
+	"pluginInfoAboutPluginFunctionTitle":"Pluginで利用可能なfunction",
 	"pluginInfoAboutMethodExecute":"execute(server, client, params)",
 	"pluginInfoAboutMethodExecuteMessage":"このメソッドは、commandで指定されたコマンドがIRCクライアントから送信された時に実行されます。serverはgetChannel等でサーバの情報を取得できます。clientは送信元の情報が取得できます。paramsはIRCクライアントから送信された情報をスペースで分割した配列です。",
 	"pluginInfoAboutMethodSetTimeout":"setTimeout(function, delayTime)",
